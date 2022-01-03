9835b8d13842642d88a4614064add8b166f846b5
Kan Yu <WalterYeYint@gmail.com>
2022-01-03 17:24:08 +0630

    feat: display sidebars and the selected data

--- flower_app.py
+++ flower_app.py
@@ -4,12 +4,35 @@ from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
 
 #---------------------------------#
+# Defining sidebar sliders
+def user_input_features():
+	sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.4)
+	sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4)
+	petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3)
+	petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 0.2)
+	data = {'sepal_length': sepal_length,
+					'sepal_width': sepal_width,
+					'petal_length': petal_length,
+					'petal_width': petal_width}
+	features = pd.DataFrame(data, index=[0])
+	return features
+
+#---------------------------------#
 # Page layout
 st.write("""
 # Iris Flower Prediction App
 This app predicts the **Iris flower** type!
 """)
 st.sidebar.header('User Input Parameters')
+
+#---------------------------------#
+# Displaying sidebars
+df = user_input_features()
+
+#---------------------------------#
+# Displaying data
+st.subheader('User Input parameters')
+st.write(df)
 
 #---------------------------------#
 
